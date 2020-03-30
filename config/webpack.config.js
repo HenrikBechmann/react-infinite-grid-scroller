24389d6544c3929f86b2b657712e30130e3b32ce
Henrik Bechmann <henrik@therefore.ca>
2020-03-30 14:42:24 -0400

    add config files

---
+++ config/webpack.config.js
@@ -0,0 +1,32 @@
+// webpack.config.js
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: {
+    main:'./src/infinitegridscroller.tsx'
+  },
+  output: {
+    filename: 'build.js',
+    path: path.resolve('lib'),
+    library:'Scroller',
+    libraryTarget:'umd'
+  },
+  resolve: {
+    extensions: ['.tsx'],
+    modules: ['src', 'node_modules']
+  },
+  module: {
+    rules: [
+      { 
+          test: /\.tsx?$/, 
+          use:['babel-loader','ts-loader']
+      },
+    ]
+  },
+  externals: {
+      "react": "React",
+      "react-dom": "ReactDOM"
+  },
+};
