285abb8e250ddb426364bdfd1e5101af89e8c75c
Henrik Bechmann <henrik@therefore.ca>
2020-04-04 01:36:17 -0400

    change viewport css

--- src/viewport.tsx
+++ src/viewport.tsx
@@ -38,8 +38,12 @@ const Viewport = ({
     const divlinerstyleRef = useRef(
         Object.assign({
         position:'absolute',
-        height:'100%',
-        width:'100%',
+        top:0,
+        right:0,
+        bottom:0,
+        left:0,
+        // height:'100%',
+        // width:'100%',
         overflow:'auto',
         backgroundColor:'red',
     } as React.CSSProperties,styles?.viewport))
