715ded9a3cdcbc7013706f7629a0f13316f715da
Henrik Bechmann <henrik@therefore.ca>
2021-06-25 00:02:48 -0400

    annotation

--- src/infinitegridscroller.tsx
+++ src/infinitegridscroller.tsx
@@ -5,6 +5,7 @@
     TODO:
     - avoid double load of nested scrollers
     - fix nested scroll back reference mismatch
+    - conflict between reposition and reparent?
     
     - promote system constants to 'advanced' parameter, eg RESIZE_TIMEOUT_FOR_ONAFTERSRESIZE
     - break up cradle
