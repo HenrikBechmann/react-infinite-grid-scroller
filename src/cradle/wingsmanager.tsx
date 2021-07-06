86f597798b5a3974e3eb9512244547a8db018ba1
Henrik Bechmann <henrik@therefore.ca>
2021-07-06 19:22:43 -0400

    wingsmanager

---
+++ src/cradle/wingsmanager.tsx
@@ -0,0 +1,13 @@
+// wingsmanager.tsx
+// copyright (c) 2021 Henrik Bechmann, Toronto, Licence: MIT
+
+import CradleManagement from './cradlemanagement'
+
+export default class WingsManager extends CradleManagement{
+
+   constructor(commonPropsRef) {
+
+      super(commonPropsRef)
+
+   }
+}
