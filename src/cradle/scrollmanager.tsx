64f0ae043ee0c16722a160c34f01ad2d7685a254
Henrik Bechmann <henrik@therefore.ca>
2022-01-03 10:16:51 -0500

    debug

--- src/cradle/scrollmanager.tsx
+++ src/cradle/scrollmanager.tsx
@@ -161,7 +161,7 @@ export default class ScrollManager {
 
                         this.updateReferenceData()
                         
-                        contentManager.updateCradleContent([], 'endofscroll') // for Safari to compensate for overscroll
+                        // contentManager.updateCradleContent([], 'endofscroll') // for Safari to compensate for overscroll
 
                     }
 
