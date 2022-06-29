50be5ad2adf0dff34a1beb2229a290dd2b4fa48c
Henrik Bechmann <henrik@therefore.ca>
2022-06-29 19:03:46 -0400

    code maintenance

--- src/cradle/servicehandler.tsx
+++ src/cradle/servicehandler.tsx
@@ -61,7 +61,7 @@ export default class ServiceHandler {
         // signals.pauseCradleIntersectionObserver = true
         // signals.pauseTriggerlinesObserver = true
         // signals.pauseScrollingEffects = true
-        // stateHandler.setCradleState('reload')
+        stateHandler.setCradleState('reload')
 
     }
 
