17b318aaf29aeab2e320f562ee49b73840fcf061
Henrik Bechmann <henrik@therefore.ca>
2022-07-31 11:00:45 -0400

    notes

--- src/InfiniteGridScroller.tsx
+++ src/InfiniteGridScroller.tsx
@@ -7,7 +7,6 @@
     ROADMAP:
         organize return values of getItem
         checkmark enable console feedback (for external callbacks)
-        constrain preload by cacheMax!
         organize feedback for all cache operations
 
         also surface external callbacks with return values for testing
@@ -22,10 +21,8 @@
         release to npm
 
     BUGS: 
-        - changing caching through call has stopped resetting reference position on boundary
 
     TODO:
-        - for preload, try to keep existing cache
         - try to preload all children, even if cached
         - cacheMax is boundary for preload, dense, and keepload
 
@@ -42,7 +39,6 @@
         - call matchCacheToCradle through contentHandler (?) iac rationalize calls to cacheHandler
         - test and review itemID
         - review state change chains in cradle
-        - check preload intent against state machine handling
         - rationalize await handling everywhere to behave like promises
         - check number of passes to scrollblock; consider implementing named states
         - error handling for preload items -- allow recovery
