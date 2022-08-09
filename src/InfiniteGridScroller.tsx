376efe8d2b3b0d9d7a80394411801f41a429c55e
Henrik Bechmann <henrik@therefore.ca>
2022-08-09 19:46:30 -0400

    debug

--- src/InfiniteGridScroller.tsx
+++ src/InfiniteGridScroller.tsx
@@ -20,6 +20,7 @@
 
         - synch of portalMap with metadataMap causes InPortal error
             - reproduce with load generic followed by test remap indexes
+            - caused by portalMap.delete x 2 to match metadataMap.delete
         - opening blockscrollpos is wrong for root list
         - viewportelementscrollpos is NaN
 
