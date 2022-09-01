6b92867e95814ec2fa32ac999c8188868829816f
Henrik Bechmann <henrik@therefore.ca>
2022-09-01 18:48:13 -0400

    code maintenance

--- src/cradle/contenthandler.tsx
+++ src/cradle/contenthandler.tsx
@@ -4,12 +4,12 @@
 import React from 'react'
 
 import { 
-    getCellFrameComponentList, 
-    calcContentShift,
     getContentListRequirements,
     getShiftInstruction,
+    calcContentShift,
     allocateContentList,
     deletePortals,
+    getCellFrameComponentList, 
 
 } from './contentfunctions'
 
