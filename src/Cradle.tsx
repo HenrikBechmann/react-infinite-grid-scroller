d5794b73c308b98d199283fbcbc6ee1aeb250bd5
Henrik Bechmann <henrik@therefore.ca>
2022-11-20 10:49:33 -0500

    code maintenance

--- src/Cradle.tsx
+++ src/Cradle.tsx
@@ -1207,42 +1207,6 @@ const Cradle = ({
                 break
             }
 
-            case 'refreshDOMsetforvariability': {
-
-                setCradleState('preparesetforvariability')
-
-                break
-
-            }
-
-            case 'preparesetforvariability': {
-
-                // avoid state conflice from onAfterScroll
-                interruptHandler.finishingSetForVariability = true
-                setTimeout(() => { // give time for DOM to produce layout
-            
-                    if (isMountedRef.current) {
-
-                        contentHandler.adjustScrollblockForVariability('setcradle')
-
-                        setCradleState('finishsetforvariability')
-                        
-                    }
-
-                }, VARIABLE_MEASUREMENTS_TIMEOUT)
-                
-                break
-
-            }
-
-            case 'finishsetforvariability': {
-
-                interruptHandler.finishingSetForVariability = false
-                setCradleState('preparerender')
-                
-                break
-            }
-
             case 'preparerender': { // cycle for DOM update
 
                 // triggerlines will have been assigned to a new triggerCell by now.
@@ -1313,6 +1277,46 @@ const Cradle = ({
 
                 break
             }
+
+            // ---------------------[ adjust scrollblock for set variable content ]--------------
+
+            case 'refreshDOMsetforvariability': {
+
+                setCradleState('preparesetforvariability')
+
+                break
+
+            }
+
+            case 'preparesetforvariability': {
+
+                // avoid state conflice from onAfterScroll
+                interruptHandler.finishingSetForVariability = true
+                setTimeout(() => { // give time for DOM to produce layout
+            
+                    if (isMountedRef.current) {
+
+                        contentHandler.adjustScrollblockForVariability('setcradle')
+
+                        setCradleState('finishsetforvariability')
+                        
+                    }
+
+                }, VARIABLE_MEASUREMENTS_TIMEOUT)
+                
+                break
+
+            }
+
+            case 'finishsetforvariability': {
+
+                interruptHandler.finishingSetForVariability = false
+                setCradleState('preparerender')
+                
+                break
+            }
+
+            // ------------------------[ adjust scrollblock for update variable content ]--------------
 
             case 'refreshDOMupdateforvariability': {
 
