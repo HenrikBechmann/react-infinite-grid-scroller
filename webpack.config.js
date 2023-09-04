46982fa4b3731da5c80992eba27ab8e9c01ae9cb
Henrik Bechmann <henrik@therefore.ca>
2023-09-04 11:15:49 -0400

    tidy up from svg experiment

--- webpack.config.js
+++ webpack.config.js
@@ -34,17 +34,6 @@ module.exports = {
             },
           },
         ],
-      },
-      {
-        test: /\.svg$/,
-        use: [
-          {
-            loader: 'svg-url-loader',
-            options: {
-              limit: 10000,
-            },
-          },
-        ],
       }
       ]
   },
