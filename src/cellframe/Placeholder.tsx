48bc4b634857705a1ceb0bf1fc481f85bf92c228
Henrik Bechmann <henrik@therefore.ca>
2023-09-09 12:10:34 -0400

    correction

--- src/cellframe/Placeholder.tsx
+++ src/cellframe/Placeholder.tsx
@@ -45,7 +45,7 @@ const Placeholder = ({
             overflow:'hidden',
         }
         const linerStyles = {
-            position:'absolute',
+            // position:'absolute',
             top:0,
             left:0,
             padding:'3px',
