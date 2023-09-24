effd5364214728f5c08f63aa3a2fe1170f6917fb
Henrik Bechmann <henrik@therefore.ca>
2023-09-23 22:55:46 -0400

    separate module for RigsDnd

--- src/InfiniteGridScroller.tsx
+++ src/InfiniteGridScroller.tsx
@@ -92,110 +92,11 @@ type RIGS = {
 // React support
 import React, { useEffect, useState, useCallback, useRef, useContext, FC } from 'react'
 
-// dnd support
-
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
-import { TouchBackend } from 'react-dnd-touch-backend'
-
-const ismobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
-
-const DndBackend = ismobile?TouchBackend:HTML5Backend
-
-const hasNativeElementsFromPoint =
-  document && (document['elementsFromPoint'] || document['msElementsFromPoint'])
-
-function getDropTargetElementsAtPoint(x, y, dropTargets) {
-  return dropTargets.filter((t) => {
-    const rect = t.getBoundingClientRect()
-    return (
-      x >= rect.left && x <= rect.right && y <= rect.bottom && y >= rect.top
-    )
-  })
-}
-
-export const MasterDndContext = React.createContext(
-    {
-        dnd:false, 
-        active:false, 
-        enabled:false,
-        scrollerID:null, 
-        setViewportState:null, // the master viewport, for rendering the dragbar
-        setDragBarState:null, // for master dragbar, to update icons
-        dropCount:0,
-        dragData:{
-            isDragging:false,
-            canDrop:false,
-            itemID:null,
-            index:null,
-            dndOptions:{} as GenericObject,
-            sourceCacheAPI:null,
-            sourceStateHandler:null,
-            sourceServiceHandler:null,
-        }
-    }) // inform children; tree scope
-
-export const ScrollerDndContext = React.createContext(null) // scroller scope
-
-// wrapper for Dnd provider - the export statement for this is next to RigsWrapper export statement below
-const RigsDnd = (props) => { // must be loaded as root scroller by host to set up Dnd provider
-
-    const masterDndContext = useContext(MasterDndContext)
-
-    const { dndOptions } = props
-
-    useEffect(()=>{
-
-        let isEnabled = dndOptions?.enabled
-
-        isEnabled = isEnabled ?? true
-
-        if (!masterDndContext.dnd) masterDndContext.dnd = true
-
-        if (!(masterDndContext.enabled === isEnabled)) {
-            masterDndContext.enabled = isEnabled
-        }
-
-        return () => {
-            Object.assign(masterDndContext,{
-                dnd:false,
-                active:false,
-                enabled:false,
-                scrollerID:null,
-                setViewportState:null,
-                setDragBarState:null,
-                dropCount:0,
-                dragData:{
-                    isDragging:false,
-                    itemID:null,
-                    index:null,
-                    dndOptions:{} as GenericObject,
-                    sourceCacheAPI:null,
-                    sourceStateHandler:null,
-                    sourceServiceHandler:null,
-                }
-            })
-        }
-
-    },[masterDndContext,dndOptions])
-
-    const enhancedProps = {...props, isDndMaster:true}
-
-    return <DndProvider backend={DndBackend} options = {backendOptions}>
-        <InfiniteGridScroller {...enhancedProps} />
-    </DndProvider>
-
-}
+import { RigsDnd } from './infinitegridscroller/rigsdnd'
 
 export { RigsDnd }
 
-// use custom function only if elementsFromPoint is not supported
-const backendOptions = {
-  getDropTargetElementsAtPoint: !hasNativeElementsFromPoint && getDropTargetElementsAtPoint
-}
-
-// idSafariIOS
-
+// isSafariIOS
 const isSafariIOSFn = () => {
     const
         is_ios = /iP(ad|od|hone)/i.test(window.navigator.userAgent),
@@ -233,7 +134,30 @@ import PortalCache from './PortalCache'
 // global session ID generator
 let globalScrollerID = 0
 
-const InfiniteGridScroller = (props) => {
+export const MasterDndContext = React.createContext(
+    {
+        dnd:false, 
+        active:false, 
+        enabled:false,
+        scrollerID:null, 
+        setViewportState:null, // the master viewport, for rendering the dragbar
+        setDragBarState:null, // for master dragbar, to update icons
+        dropCount:0,
+        dragData:{
+            isDragging:false,
+            canDrop:false,
+            itemID:null,
+            index:null,
+            dndOptions:{} as GenericObject,
+            sourceCacheAPI:null,
+            sourceStateHandler:null,
+            sourceServiceHandler:null,
+        }
+    }) // inform children; tree scope
+
+export const ScrollerDndContext = React.createContext(null) // scroller scope
+
+const InfiniteGridScroller = (props) => { // exported to 
 
     // state
     const [scrollerState, setScrollerState] = useState('setup') // setup, setlistprops, ready
---
+++ src/infinitegridscroller/rigsdnd.tsx
@@ -0,0 +1,86 @@
+// rigsdnd.tsx
+// copyright (c) 2019-2023 Henrik Bechmann, Toronto, Licence: MIT
+
+import React, { 
+    useEffect, 
+    useContext, 
+} from 'react'
+
+import InfiniteGridScroller, { MasterDndContext, GenericObject } from '../InfiniteGridScroller'
+
+// dnd support
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { TouchBackend } from 'react-dnd-touch-backend'
+
+const ismobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
+
+const DndBackend = ismobile?TouchBackend:HTML5Backend
+
+// recommended...
+const hasNativeElementsFromPoint =
+  document && (document['elementsFromPoint'] || document['msElementsFromPoint'])
+
+function getDropTargetElementsAtPoint(x, y, dropTargets) {
+  return dropTargets.filter((t) => {
+    const rect = t.getBoundingClientRect()
+    return (
+      x >= rect.left && x <= rect.right && y <= rect.bottom && y >= rect.top
+    )
+  })
+}
+
+// wrapper for Dnd provider - the export statement for this is next to RigsWrapper export statement below
+export const RigsDnd = (props) => { // must be loaded as root scroller by host to set up Dnd provider
+
+    const masterDndContext = useContext(MasterDndContext)
+
+    const { dndOptions } = props
+
+    useEffect(()=>{
+
+        let isEnabled = dndOptions?.enabled
+
+        isEnabled = isEnabled ?? true
+
+        if (!masterDndContext.dnd) masterDndContext.dnd = true
+
+        if (!(masterDndContext.enabled === isEnabled)) {
+            masterDndContext.enabled = isEnabled
+        }
+
+        return () => {
+            Object.assign(masterDndContext,{
+                dnd:false,
+                active:false,
+                enabled:false,
+                scrollerID:null,
+                setViewportState:null,
+                setDragBarState:null,
+                dropCount:0,
+                dragData:{
+                    isDragging:false,
+                    itemID:null,
+                    index:null,
+                    dndOptions:{} as GenericObject,
+                    sourceCacheAPI:null,
+                    sourceStateHandler:null,
+                    sourceServiceHandler:null,
+                }
+            })
+        }
+
+    },[masterDndContext,dndOptions])
+
+    const enhancedProps = {...props, isDndMaster:true}
+
+    return <DndProvider backend={DndBackend} options = {backendOptions}>
+        <InfiniteGridScroller {...enhancedProps} />
+    </DndProvider>
+
+}
+
+// use custom function only if elementsFromPoint is not supported
+const backendOptions = {
+  getDropTargetElementsAtPoint: !hasNativeElementsFromPoint && getDropTargetElementsAtPoint
+}
