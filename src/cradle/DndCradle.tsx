889ca4e7d29dc44a1450214e68b2fb800d69656e
Henrik Bechmann <henrik@therefore.ca>
2023-10-04 18:59:09 -0400

    code maintenance

--- src/cradle/DndCradle.tsx
+++ src/cradle/DndCradle.tsx
@@ -17,17 +17,16 @@ import React, {useEffect, useContext, useRef } from 'react'
 import { useDrop, DropTargetMonitor } from 'react-dnd'
 
 import { MasterDndContext, ScrollerDndContext, GenericObject } from '../InfiniteGridScroller'
+import { ViewportContext } from '../Viewport'
 
 import { Cradle } from '../Cradle'
-
-import { ViewportContext } from '../Viewport'
 
 // HoC for DnD functionality
 const DndCradle = (props) => {
 
     const 
-        scrollerDndContext = useContext(ScrollerDndContext),
         masterDndContext = useContext(MasterDndContext),
+        scrollerDndContext = useContext(ScrollerDndContext),
         viewportContext = useContext(ViewportContext),
 
         handlerListRef = useRef(null),
@@ -36,15 +35,16 @@ const DndCradle = (props) => {
         { scrollerID, virtualListSpecs } = props,
         { size:listsize } = virtualListSpecs
 
-    const [ targetData, targetConnector ] = useDrop({
+    // const [ targetData, targetConnector ] = useDrop({
+    const [ , targetConnector ] = useDrop({
         accept:scrollerDndContext.dndOptions.accept || ['-x-x-x-'],
-        collect:(monitor:DropTargetMonitor) => {
-            return {
-                item:monitor.getItem() as any,
-                isOver:monitor.isOver(),
-                canDrop:monitor.canDrop(),
-            }
-        },
+        // collect:(monitor:DropTargetMonitor) => {
+        //     return {
+        //         item:monitor.getItem() as any,
+        //         isOver:monitor.isOver(),
+        //         canDrop:monitor.canDrop(),
+        //     }
+        // },
         drop:(item:GenericObject,monitor) => {
 
             const dropResult:GenericObject = monitor.getDropResult()
@@ -65,24 +65,30 @@ const DndCradle = (props) => {
                 fromIndex = item.index,
                 toIndex = dropResult.target.index
 
-            if (item.scrollerID === dropResult.target.scrollerID) {
+            if (item.scrollerID === dropResult.target.scrollerID) { // intra-list
 
                 serviceHandler.moveIndex(toIndex, fromIndex)
-                scrollerDndContext.displacedIndex = (fromIndex > toIndex)? toIndex + 1:toIndex - 1
 
-            } else {
+                scrollerDndContext.displacedIndex = 
+                    (fromIndex > toIndex)? 
+                        toIndex + 1:
+                        toIndex - 1
+
+            } else { // inter-list
 
                 const { dragData } = masterDndContext
                 const sourceProps = dragData.sourceServiceHandler.getPropertiesSnapshot()
-                const sourcelistsize = sourceProps.virtualListProps.size
+                const { size:sourcelistsize } = sourceProps.virtualListProps
                 const remove = -1
 
-                dragData.sourceCacheAPI.insertRemoveIndex(fromIndex, fromIndex, remove, sourcelistsize, false) // false = removeItems (not)
+                // last argument false = do not remove Items
+                dragData.sourceCacheAPI.insertRemoveIndex(fromIndex, fromIndex, remove, sourcelistsize, false) 
                 dragData.sourceServiceHandler.newListSize = sourcelistsize - 1
                 dragData.sourceStateHandler.setCradleState('changelistsizeafterinsertremove')
 
+                // make space for insert
                 const insert = +1
-                const pendingChangesList = cacheAPI.insertRemoveIndex(toIndex, toIndex, insert, listsize) // make space for insert
+                const pendingChangesList = cacheAPI.insertRemoveIndex(toIndex, toIndex, insert, listsize)
                 // pendingChangesList = [
                 //     startChangeIndex, 
                 //     rangeincrement, 
@@ -95,17 +101,22 @@ const DndCradle = (props) => {
                 const [startChangeIndex, rangeincrement, cacheIndexesShiftedList] = pendingChangesList
 
                 const fromScroller = item.scrollerID
-                cacheAPI.transferPortalMetadataToScroller(
-                    item.itemID, toIndex) // move into space
+                cacheAPI.transferPortalMetadataToScroller( item.itemID, toIndex ) // move into space
+
                 contentHandler.synchronizeCradleItemIDsToCache(
                     cacheIndexesShiftedList, rangeincrement, startChangeIndex) // sync cradle
+
                 serviceHandler.newListSize = listsize + rangeincrement // rangeincrement always +1 here
+
                 stateHandler.setCradleState('applyinsertremovechanges') // re-render
+
                 scrollerDndContext.displacedIndex = toIndex + 1
 
             }
+
             scrollerDndContext.droppedIndex = toIndex
             scrollerDndContext.sourceItem = item
+            
         },
     })
 
