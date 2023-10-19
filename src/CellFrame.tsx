e7b3f637452c2d7c5aa2e7399dd245866fcb286d
Henrik Bechmann <henrik@therefore.ca>
2023-10-18 21:18:26 -0400

    code maintenance

--- src/CellFrame.tsx
+++ src/CellFrame.tsx
@@ -533,7 +533,7 @@ export const CellFrame = ({
 
                             returnvalue = usercontent = undefined
                             if (!itempack) {
-                                console.log('error', e)
+                                // console.log('error', e)
                                 error = new Error ('no data ( ' + e.message +')')
                             } else {
                                 error = e
